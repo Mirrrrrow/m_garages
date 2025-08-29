87fd9f51f7b14b166e9e3e53afe39f006e199c87
Mirow <maxim.doerr@icloud.com>
2025-08-29 15:45:33 +0200

    refactor(client/utils): extract and reorganize utility functions>
    Refactors utility functions by moving them from client/main.lua
    to a dedicated client/utils.lua for better modularity and maintenance.
    Removing unnecessary code and allowing Utils module to be directly used
    with functions like addBlip and spawnPed.

--- client/main.lua
+++ client/main.lua
@@ -1,64 +1,15 @@
-local spawnedPeds = lib.array:new()
-local target = exports.ox_target
-
 Utils = require 'client.utils'
 MenuBuilder = require 'client.menuBuilder'
 ModalBuilder = require 'client.modalBuilder'
 Actions = require 'client.actions'
 
-
----@param data { label: string, coords: vector3, sprite: number, color: number }
-local function addBlip(data)
-    local coords = data.coords
-    local blip = AddBlipForCoord(coords.x, coords.y, coords.z)
-
-    SetBlipSprite(blip, data.sprite)
-    SetBlipColour(blip, data.color)
-    SetBlipScale(blip, 0.8)
-    SetBlipAsShortRange(blip, true)
-    BeginTextCommandSetBlipName('STRING')
-    AddTextComponentString(data.label)
-    EndTextCommandSetBlipName(blip)
-end
-
----@param data { coords: vector4, model: number|string, scenario: string }
-local function spawnPed(data)
-    local model = lib.requestModel(data.model)
-    if not model then lib.print.error(('Unkown ped model: \'%s\'.'):format(data.model)) end
-
-    local coords = data.coords
-    local entity = CreatePed(0, model, coords.x, coords.y, coords.z, coords.w, false, true)
-
-    local scenario = data.scenario
-    if scenario then
-        TaskStartScenarioInPlace(entity, scenario, 0, true)
-    end
-
-    SetModelAsNoLongerNeeded(model)
-    FreezeEntityPosition(entity, true)
-    SetEntityInvincible(entity, true)
-    SetBlockingOfNonTemporaryEvents(entity, true)
-
-    spawnedPeds:push(entity)
-
-    return entity
-end
-
-local function cleanPeds()
-    spawnedPeds:forEach(function(entity)
-        if DoesEntityExist(entity) then
-            SetEntityAsMissionEntity(entity, false, true)
-            DeleteEntity(entity)
-        end
-    end)
-end
-
+local target = exports.ox_target
 for key, garage in pairs(Config.garages) do
     local coords = garage.coords
 
     local blip = garage.blip
     if blip then
-        addBlip({
+        Utils.addBlip({
             label = blip.label or garage.label,
             coords = coords.xyz,
             sprite = blip.sprite,
@@ -73,7 +24,7 @@ for key, garage in pairs(Config.garages) do
         onEnter = function(self)
             if self.entity then return end
 
-            local entity = spawnPed({
+            local entity = Utils.spawnPed({
                 coords = coords,
                 model = ped.model,
                 scenario = ped.scenario
@@ -96,16 +47,7 @@ for key, garage in pairs(Config.garages) do
             target:removeLocalEntity(entity)
             DeleteEntity(entity)
 
-            spawnedPeds = spawnedPeds:filter(function(element)
-                return element ~= entity
-            end)
-
             self.entity = nil
         end
     })
 end
-
-AddEventHandler('onResourceStop', function(resource)
-    if resource ~= cache.resource then return end
-    cleanPeds()
-end)
--- client/utils.lua
+++ client/utils.lua
@@ -1,4 +1,51 @@
 local Utils = {}
+local spawnedPeds = lib.array:new()
+
+local function cleanPeds()
+    spawnedPeds:forEach(function(entity)
+        if DoesEntityExist(entity) then
+            SetEntityAsMissionEntity(entity, false, true)
+            DeleteEntity(entity)
+        end
+    end)
+end
+
+---@param data { label: string, coords: vector3, sprite: number, color: number }
+function Utils.addBlip(data)
+    local coords = data.coords
+    local blip = AddBlipForCoord(coords.x, coords.y, coords.z)
+
+    SetBlipSprite(blip, data.sprite)
+    SetBlipColour(blip, data.color)
+    SetBlipScale(blip, 0.8)
+    SetBlipAsShortRange(blip, true)
+    BeginTextCommandSetBlipName('STRING')
+    AddTextComponentString(data.label)
+    EndTextCommandSetBlipName(blip)
+end
+
+---@param data { coords: vector4, model: number|string, scenario: string }
+function Utils.spawnPed(data)
+    local model = lib.requestModel(data.model)
+    if not model then lib.print.error(('Unkown ped model: \'%s\'.'):format(data.model)) end
+
+    local coords = data.coords
+    local entity = CreatePed(0, model, coords.x, coords.y, coords.z, coords.w, false, true)
+
+    local scenario = data.scenario
+    if scenario then
+        TaskStartScenarioInPlace(entity, scenario, 0, true)
+    end
+
+    SetModelAsNoLongerNeeded(model)
+    FreezeEntityPosition(entity, true)
+    SetEntityInvincible(entity, true)
+    SetBlockingOfNonTemporaryEvents(entity, true)
+
+    spawnedPeds:push(entity)
+
+    return entity
+end
 
 function Utils.generateCarLabel(model)
     model = tonumber(model)
@@ -25,5 +72,10 @@ function Utils.calculateWithdrawalFees(garageKey, model)
         (parkingFeesData.multipliers.models[model] or parkingFeesData.multipliers[GetVehicleClassFromName(model)] or 1)
     return fee
 end
+
+AddEventHandler('onResourceStop', function(resource)
+    if resource ~= cache.resource then return end
+    cleanPeds()
+end)
 
 return Utils
